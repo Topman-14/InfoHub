f8819644303781b09b1a4c025db9fb83feaeac5e
Oluwagbemiga <samuelifeoluwa540@gmail.com>
2023-03-14 20:07:49 +0100

    Readjust files for contribution

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,6 @@
+document.querySelectorAll('.nav-link').forEach(link =>{
+   if (link.href === window.location.href){
+    link.setAttribute('aria-current', 'page') }
+})
 
+//=== Contributed Js Code ===
