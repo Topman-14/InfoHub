8f0f40cfd6c16126cf19dd359068c7f45ab1c090
Topman-14 <talktotimothy101@gmail.com>
2023-06-04 00:05:58 +0100

    fix development bugs

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,19 @@
 const mainNavbar = document.querySelector('#main-navbar');
-
+const menuBtn = document.querySelector('#menu_close');
+const mobileMenu = document.querySelector('#menu');
 var prevScrollpos = window.pageYOffset;
+menuBtn.addEventListener('click', ()=>{
+    menuBtn.style.opacity = '0'
+    mobileMenu.style.display = 'flex'
+    mobileMenu.style.animation = 'slide-out 0.7s'
+})
+mobileMenu.addEventListener('click', ()=>{
+    mobileMenu.style.animation = 'slide-in 0.7s'
+    menuBtn.style.opacity = '1';
+    setTimeout(()=>{
+        mobileMenu.style.display = 'none';
+    }, 650)
+})
 
 window.onscroll = ()=>{
     var currentScrollPos = window.pageYOffset;
